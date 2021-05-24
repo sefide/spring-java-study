3135987046ad29c77835367d36ef776ce4d905b4
김다희 <sefide@naver.com>
2021-05-24 15:23:57 +0900

    refactor: AsyncApplication main 메소드 정리

--- src/main/java/com/heedi/spring/async/AsyncApplication.java
+++ src/main/java/com/heedi/spring/async/AsyncApplication.java
@@ -1,13 +1,27 @@
 package com.heedi.spring.async;
 
 public class AsyncApplication {
+    private static FixedThreadProcessor fixedThreadProcessor;
+    private static CompletableFutureProcessor completableFutureProcessor;
 
     public static void main(String[] args) {
-//        FixedThreadProcessor fixedThreadProcessor = new FixedThreadProcessor();
-//        fixedThreadProcessor.execute();
+        FixedThreadProcessor();
+        executeCompletableFutureTest();
+        executeCompletableFutureFailTest();
+    }
 
-        CompletableFutureProcessor completableFutureProcessor = new CompletableFutureProcessor();
-//        completableFutureProcessor.execute();
+    private static void FixedThreadProcessor() {
+        fixedThreadProcessor = new FixedThreadProcessor();
+        fixedThreadProcessor.execute();
+    }
+
+    private static void executeCompletableFutureTest() {
+        completableFutureProcessor = new CompletableFutureProcessor();
+        completableFutureProcessor.execute();
+    }
+
+    private static void executeCompletableFutureFailTest() {
+        completableFutureProcessor = new CompletableFutureProcessor();
         completableFutureProcessor.executeFail();
     }
 }
